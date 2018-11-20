4fdc04e20b5263dcbb6a38ed9b82c0c717923b71
armandoGutierrez <gutierrezjarr@gmail.com>
2018-11-20 10:01:50 -0800

    Initial commit

---
+++ Assets/M2MqttGVR/Scripts/PlayerWalk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour {
+
+	public int playerSpeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        //Debug.Log("Updated...");
+	}
+}
